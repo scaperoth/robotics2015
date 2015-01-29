42f02469ab9203fbea53ea064f9d180cb093ed1d
Matt Scaperoth <mscapero@gwu.edu>
2015-01-29 15:19:42 -0500

    added forward kinematics notes

---
+++ hw/hw1/feedback_control/PID_hold.cpp
@@ -0,0 +1,119 @@
+#include <boost/bind.hpp>
+#include <gazebo/gazebo.hh>
+#include <gazebo/physics/physics.hh>
+#include <gazebo/common/common.hh>
+#include <gazebo/transport/transport.hh>
+#include <gazebo/msgs/msgs.hh>
+#include <gazebo/sensors/SensorManager.hh>
+#include <gazebo/sensors/RaySensor.hh>
+#include <stdio.h>
+#include <fstream>
+
+namespace gazebo
+{
+class PIDController : public ModelPlugin
+{
+private: physics::WorldPtr _world;
+private: physics::ModelPtr _model;
+private: physics::JointPtr _j1, _j2;
+private: std::ofstream _output_des, _output_current;
+
+public: PIDController()
+    {
+    }
+
+public: ~PIDController()
+    {
+        _output_current.close();
+        _output_des.close();
+    }
+
+public: void Load(physics::ModelPtr _parent, sdf::ElementPtr _sdf)
+    {
+        // Store the pointer to the model
+        _model = _parent;
+
+        // store the pointer to the world
+        _world = _model->GetWorld();
+
+        // Listen to the update event. This event is broadcast every
+        // simulation iteration.
+        this->updateConnection = event::Events::ConnectWorldUpdateBegin(
+                                     boost::bind(&PIDController::OnUpdate, this, _1));
+
+        // get the joints
+        this->_j1 = _model->GetJoint("upper_joint");
+        this->_j2 = _model->GetJoint("lower_joint");
+    }
+
+    // open up the file for writing
+public: void Init()
+    {
+        _output_current.open("PID_hold.state");
+        _output_des.open("PID_hold.desired");
+
+        // set initial joint positions
+        this->_j1->SetPosition(0, -M_PI_2);
+        this->_j2->SetPosition(0, 0.0);
+    }
+
+    // Called by the world update start event
+public: void OnUpdate(const common::UpdateInfo & /*_info*/)
+    {
+        const double CHANGEME = 0.0;
+
+        static double sum_p1_err = 0.0;
+        static double sum_p2_err = 0.0;
+
+        // set the desired positions and velocities
+        const double J1_DES = 0.0, J2_DES = 0.0;
+        const double vJ1_DES = 0.0, vJ2_DES = 0.0;
+
+        // TODO: setup gains
+        const double KP = 1400, KV = 450, KI = .9;
+
+        // output current and desired state
+        _output_current << _j1->GetAngle(0).Radian() << " " << _j2->GetAngle(0).Radian() << std::endl;
+        _output_des << J1_DES << " " << J2_DES << std::endl;
+
+        // get current positions and velocities
+        double theta1 = _j1->GetAngle(0).Radian();
+        double dtheta1 = _j1->GetVelocity(0);
+        double theta2 = _j2->GetAngle(0).Radian();
+        double dtheta2 = _j2->GetVelocity(0);
+
+        // TODO: setup position and velocity errors
+        const double perr1 = (J1_DES - theta1);
+        const double derr1 = (vJ1_DES - dtheta1);
+        const double perr2 = (J2_DES - theta2);
+        const double derr2 = (vJ2_DES - dtheta2);
+
+        //abstract values for P,I, and D portions of controller
+        double proportional1  = KP * perr1;
+        double derivative1    = KV * derr1;
+        double integral1      = KI * sum_p1_err;
+        double proportional2  = KP * perr2;
+        double derivative2    = KV * derr2;
+        double integral2      = KI * sum_p2_err;
+
+        // TODO: compute torques
+        double tau1 = proportional1 + derivative1 + integral1;
+        double tau2 = proportional2 + derivative2 + integral2;
+
+        // set torques
+        this->_j1->SetForce(0, tau1);
+        this->_j2->SetForce(0, tau2);
+
+        // TODO: update p1 and p2 error sums with position errors
+        sum_p1_err += perr1;
+        sum_p2_err += perr2;
+    }
+
+    // Pointer to the update event connection
+private: event::ConnectionPtr updateConnection;
+};
+
+// Register this plugin with the simulator
+GZ_REGISTER_MODEL_PLUGIN(PIDController)
+}
+
---
+++ notes/fwd_kinematics.md
@@ -0,0 +1,71 @@
+##FORWARD KINEMATICS
+
+given joint angles and want to know where some point on the robot is as a funnction of those angles
+
+	- commonly looking for what's called "the end effector"
+
+			 _________________| < -- end effector example
+			/theta
+		   /
+		  /
+		 /
+		- theta
+
+###The Problem (of forward kinematics)
+		- Given the current configuration of the robot, where is some point (w.r.t. some frame)?
+		- How fast (and what direction) is the point moving?
+		- Rigid Links
+			- definition of rigid (body/links/etc): pick any two points on the body, and the distance between any two points on the rigid body remains constant over time.
+		- Example:
+			- How do we translate something like this:
+			_______________________________
+		   |							   |
+		   |			x(t) ------------->| P
+		   |________________________u______|							   |
+
+		   - x is center of mass and in the global frame, P is in the global frame and U is constant in the local frame
+		   - P. = X^ + Ru 
+		   - where R is a rotation matrix from global to local
+
+		   - Using a 4x4 matrix:
+
+		   gTb * | u1 |    <-- global to local transformation then multiply by matrix
+		   		 | u2 |	
+		   		 | 1  |
+
+###Coordinate Types
+	- minimal/independent (as shown above)
+	- robot affixed to the environment equivalent to joint space configuration 
+	- "Floating base"
+
+###Considering the first example:
+	
+			 _______q2________| < -- end effector example
+			/theta
+		q1 /
+		  /
+		 /
+		- theta
+		
+	where q1 = 	 |  x1   |   
+		   		 |  x2   |	
+		   		 | theta |
+
+	- so how do we make sure the joints are connected? 
+	- if q are vectors with another local vector u and u' that point towards the joint, we have to ensure that:
+		x1 + Ru = x2 + R'u'   OR
+		x1 + Ru - x2 + R'u' = 0
+
+	- So now we can talk about degrees of freedom!
+
+###Degrees of freedom
+	- same number of the minimal coordinates
+	- other way is to find the maximum number of representations and subtract the number of constraints
+		- (#of links * degrees-of-freedom/links) - constraint equations
+		- in the above case we have 6 - 2 * 2 = 2 degrees-of-freedom
+
+	- how many ways can you think of to constrain two bodies in three dimensions?
+	- x,y,z and roll,pitch, yaw
+
+
+
